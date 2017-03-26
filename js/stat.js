058ec4ac497cbcea56b8c457556133a2daa30f37
Alexander <lss1ash@rambler.ru>
2017-03-26 15:59:22 +1100

    Настраиваем облачко и текст

---
+++ js/stat.js
@@ -0,0 +1,22 @@
+window.renderStatistics = function(ctx, names, times) {
+  // Определим значения
+  var BALOON_X = 100;
+  var BALOON_Y = 10;
+  var BALOON_WIDTH = 420;
+  var BALOON_HEIGHT = 270;
+  // Тень под облачком
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(BALOON_X + 10, BALOON_Y + 10, BALOON_WIDTH, BALOON_HEIGHT);
+  // Облачко
+  ctx.fillStyle = 'white';
+  ctx.fillRect(BALOON_X, BALOON_Y, BALOON_WIDTH, BALOON_HEIGHT);
+  // Текст
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.textAlign = 'center';
+  ctx.fillStyle = 'black';
+  ctx.fillText('Ура, Вы победили!', BALOON_X + 20, BALOON_Y + 20);
+  ctx.fillText('Список результатов:', BALOON_X + 20, BALOON_Y + 40);
+
+
+}
