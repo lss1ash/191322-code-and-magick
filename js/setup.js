235d5e701dd95912a8ac226ce655676988503489
Alexander <lss1ash@rambler.ru>
2017-04-06 17:09:25 +1100

    Добавил изменение цветов мантии, глаз и файрбола по клику

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,7 @@
   var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var KEYCODE_ENTER = 13;
   var KEYCODE_ESC = 27;
 
@@ -31,6 +32,25 @@
   var setupSubmit = setupDialog.querySelector('.setup-submit');
   var setupUserName = setupDialog.querySelector('.setup-user-name');
   addDialogEventListeners();
+
+  var wizardCoat = setupDialog.querySelector('.wizard-coat');
+  var wizardEyes = setupDialog.querySelector('.wizard-eyes');
+  var fireball = setupDialog.querySelector('.setup-fireball-wrap');
+  wizardCoat.addEventListener('click', changeCoat);
+  wizardEyes.addEventListener('click', changeEyes);
+  fireball.addEventListener('click', changeFireball);
+
+  function changeCoat() {
+    wizardCoat.style.fill = getRandomItem(COAT_COLORS);
+  }
+
+  function changeEyes() {
+    wizardEyes.style.fill = getRandomItem(EYES_COLORS);
+  }
+
+  function changeFireball() {
+    fireball.style.backgroundColor = getRandomItem(FIREBALL_COLORS);
+  }
 
   function addDialogEventListeners() {
     setupOpenIcon.setAttribute('tabindex', 0);
